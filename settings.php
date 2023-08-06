4f21496f1decefa9b18f1fe935d6e8763b4da0ed
Cameron <cameron@purple.ai>
2023-08-06 15:35:55 +0100

    Fixed error and added styling

--- settings.php
+++ settings.php
@@ -1,6 +1,12 @@
 <?php
 
-return [
-    'api_base_url' => 'https://restcountries.com/v3.1/',
+require_once __DIR__ . '/vendor/autoload.php';
 
+// Load the .env file to set environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+    // Add .env settings from here
+    'API_BASE_URL' => $_ENV['API_BASE_URL'],
 ];
